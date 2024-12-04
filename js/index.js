2ab324c3eaf00ed9c7af28aa3425e5fba99ac895
Wargrimm <allan.lulin@cegedim-srh.com>
2024-12-04 17:58:27 +0100

    index new

--- js/index.js
+++ js/index.js
@@ -1,67 +1,29 @@
-/* besoins routeur
-public -> sur this
-    +page actuelle champs lecture
-    +changement de chemin fonction
-privé pas sur this
-    +champs de route prive en ecriture
-    +moddif url
-    +modif contenu(loadEvents)
-    +recuperation du contenu de page depuis le reseau
-*/
-function Router(rootNode,rootFolderOfTemplates = "/pages") {
-    /*definitions locales(interne) des propriétés et fonctions */
-    var currentRoute = location.pathname;
-    function changePathName(pathName) {
-      history.pushState(null, null, pathName);
-      currentRoute = location.pathname;
-    }
-    function loadContentInPage(eventLoader) {}
-    function getContentFromNetwork(contentUrl) {
-      var xhr = new XMLHttpRequest();
-      xhr.open("GET", contentUrl);
-      xhr.onreadystatechange = function (evt) {
-        if (xhr.readyState < XMLHttpRequest.DONE) {
-          return;
-        }
-        if (xhr.status >= 400) {
-          console.log("erreur" + xhr.status);
-        }
-        console.log(xhr.responseText);
-        rootNode.innerHTML=xhr.responseText;
-      };
-      xhr.send();
-    }
-  
-    /*definition des acces exterieurs a l'instance */
-    /**
-     * getter de la route current
-     * @returns {string}current pathName
-     */
-    this.getCurrentRoute = getCurrentRoute;
-    function getCurrentRoute() {
-      return currentRoute;
-    }
-  
-    /**
-     * fonction de navigation avec chargement du contenu
-     * @param {string} pathName path to navigate (start with '/')
-     */
-    this.navigate = navigate;
-    function navigate(pathName = "/") {
-      changePathName(pathName);
-      var url = rootFolderOfTemplates;
-      switch (pathName) {
-        case "/thumbnail":
-          url += "/thumbnail/thumbnail.html";
-          break;
-        case "/editor":
-          url += "/editor/editor.html";
-          break;
-        default:
-          url += "/home/home.html";
-          break;
-      }
-      getContentFromNetwork(url);
-      loadContentInPage();
-    }
+/**
+ * chargement de base de mon app
+ */
+function loadingDOM() {
+    document.querySelector("#js-notification").remove();
+    document.querySelector("#header button").remove();
+    loadNavbarEvents();
+    //loadEditorEvent();
   }
+  /**
+   * Fonction de chargement des events de navbar
+   */
+  function loadNavbarEvents() {
+    var aList = document.querySelectorAll("a");
+    // for(var i=0;i<aList.length;i++){
+    //   aList[i].addEventListener('click',function(evt){
+    //     evt.preventDefault();
+    //     console.log('navigate to ',evt.target.href);
+    //   })
+    // }
+    aList.forEach(function (element) {
+      element.addEventListener("click", (evt) => {
+        evt.preventDefault();
+        history.pushState(null,null,evt.target.href);
+      });
+    });
+  }
+  document.addEventListener("DOMContentLoaded", loadingDOM);
+  var router=new Router(document.getElementById('wrapper'));

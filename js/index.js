ebdd1eaf8351a2d8894e43ee8e4bfeb593fe4258
Wargrimm <allan.lulin@cegedim-srh.com>
2024-12-04 12:00:12 +0100

    ajout listener

--- js/index.js
+++ js/index.js
@@ -1,1 +1,10 @@
-alert("Le fichier Js n'est pas chargé");
+// alert("Le fichier Js n'est pas chargé");
+function loadingDOM() {
+  var notif = document.querySelector("#js-notification");
+  notif.style.backgroundColor = "green";
+  notif.innerHTML =
+    'le <span style="font-weight:900;color:blue;">JS</span> est ok';
+}
+
+document.addEventListener(DOMContentLoaded,loadingDOM);
+

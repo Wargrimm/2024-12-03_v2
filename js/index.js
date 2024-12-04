4c1d0004fe60a414f63d197a446061a62047ddcf
Wargrimm <allan.lulin@cegedim-srh.com>
2024-12-04 14:48:03 +0100

    lauch debug + editor form

--- js/index.js
+++ js/index.js
@@ -1,21 +1,10 @@
 function loadingDOM() {
-    var js = document.querySelector("#js-notification");
-    js.style.backgroundColor = "GREEN";
-    js.innerHTML =
-      'le <span style="font-weight:900;color :blue;">JS</span> est OK';
-    document
-      .querySelector("button.btn-danger")
-      .addEventListener("click", function (evt) {
-        evt.stopPropagation();
-        console.log('button',evt);
-        console.log("button du header clické");
-      });
-      document
-      .querySelector("#header")
-      .addEventListener("click", function (evt) {
-        console.log('header',evt);
-        console.log("header clické");
-      });
-  }
-  
-  document.addEventListener("DOMContentLoaded", loadingDOM);
+  document.querySelector("#js-notification").remove();
+  document.querySelector("#header button").remove();
+  /*document.querySelector("#header button").addEventListener("click", function (evt) {
+    console.log("header", evt);
+    console.log("header clické");
+  })*/;
+}
+
+document.addEventListener("DOMContentLoaded", loadingDOM);

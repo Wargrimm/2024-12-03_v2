d9f5a4479c200fff34bf2d81f4664a4c4c570463
Wargrimm <allan.lulin@cegedim-srh.com>
2024-12-05 17:10:45 +0100

    ajout image

--- js/editor.js
+++ js/editor.js
@@ -1,12 +1,29 @@
 function loadEditor(params) {
   console.log(params);
   loadEditorEvent();
+  promiseImage.then((arrayImages) => {
+    loadSelectImagesInForm(arrayImages);
+  });
 }
 
 function loadEditorEvent() {
-  console.log(router.getCurrentRoute())
+  console.log(router.getCurrentRoute());
   document.forms["editor-form"].addEventListener("submit", function (evt) {
     evt.preventDefault();
     console.log("form submit");
   });
 }
+
+const loadSelectImagesInForm = (images) => {
+  const select = document.forms["editor-form"]["imageId"];
+  const optionBase = select.children[0];
+  select.innerHTML = "";
+  select.appendChild(optionBase);
+  images.forEach((image) => {
+    const optionClone = optionBase.cloneNode(true);
+    optionClone.value = image.id;
+    optionClone.innerHTML = image.name;
+    select.appendChild(optionClone);
+  });
+  debugger;
+};

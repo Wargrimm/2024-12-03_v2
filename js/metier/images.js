259c4ddf53a89e8490156bc94d5c04ffe0547dc3
Wargrimm <allan.lulin@cegedim-srh.com>
2024-12-05 16:26:54 +0100

    corection meme + création image

---
+++ js/metier/images.js
@@ -0,0 +1,14 @@
+class images extends Array{
+    #endpoint = "/images"
+    constructor() {
+        super();
+    }
+    find(params) {
+        super.find(params);
+    }
+    load() {
+
+    }
+}
+let images = new images();
+images.load();
--- js/metier/meme.js
+++ js/metier/meme.js
@@ -2,50 +2,50 @@
  * class de gestion d'un meme
  */
 class Meme {
-  id = undefined;
-  text = "";
-  x = 0;
-  y = 10;
-  fontSize = 10;
-  fontWeight = 500;
-  underline = false;
-  italic = false;
-  color = "#FFFFFF";
-  imageId = -1;
-  #endpoint = "/memes";
-  /**
-   * constructor de meme
-   */
-  constructor() {
-    console.log("constructor de meme");
-  }
-  save() {
-    //console.log("save " + this.id + " at " + this.#endpoint, this);
-    // this.publicSave();
-    // this.#privateSave();
-    fetch(
-      `http://localhost:5679${this.#endpoint}${
-        undefined !== this.id ? "/" + this.id : ""
-      }`,
-      {
-        method: undefined !== this.id ? "PUT" : "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify(this),
-      }
-    )
+    id = undefined;
+    text = "";
+    x = 0;
+    y = 10;
+    fontSize = 10;
+    fontWeight = 500;
+    underline = false;
+    italic = false;
+    color = "#FFFFFF";
+    imageId = -1;
+    #endpoint = "/memes";
+    /**
+     * constructor de meme
+     */
+    constructor() {
+      console.log("constructor de meme");
+    }
+    save() {
+      fetch(
+        `http://localhost:5679${this.#endpoint}${
+          undefined !== this.id ? "/" + this.id : ""
+        }`,
+        {
+          method: undefined !== this.id ? "PUT" : "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(this),
+        }
+      )
         .then((r) => r.json())
         .then((o) => {
           Object.assign(this, o);
         });
+      // console.log("save " + this.id + " at " + this.#endpoint, this);
+      // this.publicSave();
+      // this.#privateSave();
+    }
+    publicSave() {
+      console.log("public saving");
+    }
+    #privateSave() {
+      console.log("private saving");
+    }
   }
-  publicSave() {
-    console.log("public saving");
-  }
-  #privateSave() {
-    console.log("private saving");
-  }
-}
-let meme = new Meme();
-// meme.save();
+  let meme = new Meme();
+  //meme.save();

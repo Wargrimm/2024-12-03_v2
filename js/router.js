bdcef21d82dc41e7628a7e347782ea6beca7231c
Wargrimm <allan.lulin@cegedim-srh.com>
2024-12-05 09:46:58 +0100

    passage objectroute

--- js/router.js
+++ js/router.js
@@ -8,60 +8,70 @@ privé pas sur this
     +modif contenu(loadEvents)
     +recuperation du contenu de page depuis le reseau
 */
-function Router(rootNode,rootFolderOfTemplates = "/pages") {
-    /*definitions locales(interne) des propriétés et fonctions */
-    var currentRoute = location.pathname;
-    function changePathName(pathName) {
-      history.pushState(null, null, pathName);
-      currentRoute = location.pathname;
-    }
-    function loadContentInPage(eventLoader) {}
-    function getContentFromNetwork(contentUrl) {
-      var xhr = new XMLHttpRequest();
-      xhr.open("GET", contentUrl);
-      xhr.onreadystatechange = function (evt) {
-        if (xhr.readyState < XMLHttpRequest.DONE) {
-          return;
-        }
-        if (xhr.status >= 400) {
-          console.log("erreur" + xhr.status);
-        }
-        console.log(xhr.responseText);
-        rootNode.innerHTML=xhr.responseText;
-      };
-      xhr.send();
-    }
-  
-    /*definition des acces exterieurs a l'instance */
-    /**
-     * getter de la route current
-     * @returns {string}current pathName
-     */
-    this.getCurrentRoute = getCurrentRoute;
-    function getCurrentRoute() {
-      return currentRoute;
-    }
-  
-    /**
-     * fonction de navigation avec chargement du contenu
-     * @param {string} pathName path to navigate (start with '/')
-     */
-    this.navigate = navigate;
-    function navigate(pathName = "/") {
-      changePathName(pathName);
-      var url = rootFolderOfTemplates;
-      switch (pathName) {
-        case "/thumbnail":
-          url += "/thumbnail/thumbnail.html";
-          break;
-        case "/editor":
-          url += "/editor/editor.html";
-          break;
-        default:
-          url += "/home/home.html";
-          break;
-      }
-      getContentFromNetwork(url);
-      loadContentInPage();
+function Router(rootNode, rootFolderOfTemplates = "/pages") {
+  /*definitions locales(interne) des propriétés et fonctions */
+  var currentRoute = location.pathname;
+  function changePathName(pathName) {
+    history.pushState(null, null, pathName);
+    currentRoute = location.pathname;
+  }
+  function loadContentInPage(routeObject) {
+    rootNode.innerHTML = routeObject.template;
+    if(typeof routeObject.loaderJs == 'function') {
+      routeObject.loaderJs();
     }
   }
+  function getContentFromNetwork(routeObject) {
+    var xhr = new XMLHttpRequest();
+    xhr.open("GET", routeObject.url);
+    xhr.onreadystatechange = function (evt) {
+      if (xhr.readyState < XMLHttpRequest.DONE) {
+        return;
+      }
+      if (xhr.status >= 400) {
+        console.log("erreur" + xhr.status);
+        return;
+      }
+      console.log(xhr.responseText);
+      routeObject.template = xhr.responseText;
+      loadContentInPage(routeObject);
+    };
+    xhr.send();
+  }
+
+  /*definition des acces exterieurs a l'instance */
+  /**
+   * getter de la route current
+   * @returns {string}current pathName
+   */
+  this.getCurrentRoute = getCurrentRoute;
+  function getCurrentRoute() {
+    return currentRoute;
+  }
+
+  /**
+   * fonction de navigation avec chargement du contenu
+   * @param {string} pathName path to navigate (start with '/')
+   */
+  this.navigate = navigate;
+  function navigate(pathName = "/") {
+    changePathName(pathName);
+    // var url = rootFolderOfTemplates;
+    var route = {};
+    route.url = rootFolderOfTemplates;
+    switch (pathName) {
+      case "/thumbnail":
+        route.url += "/thumbnail/thumbnail.html";
+        break;
+      case "/editor":
+        route.url += "/editor/editor.html";
+        route.loaderJs = loadEditorEvent;
+        break;
+      default:
+        route.url += "/home/home.html";
+        break;
+    }
+    getContentFromNetwork(route);
+    // loadContentInPage();
+  }
+}

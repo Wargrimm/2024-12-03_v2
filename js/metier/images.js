b44578ae41bb76b4a27080ec7d42e80c8290643a
Wargrimm <allan.lulin@cegedim-srh.com>
2024-12-05 16:55:08 +0100

    maj ortho

--- js/metier/images.js
+++ js/metier/images.js
@@ -1,23 +1,32 @@
-class images extends Array {
-  #endpoint = "/images";
-  constructor() {
-    super();
-  }
-  find(params) {
-    console.log(params);
-    super.find(params);
-  }
-  load() {
-    return fetch(`http://localhost:5679${this.#endpoint}`, {
-        headers: {Accept: "aplication/json"},
+class Images extends Array {
+    #endpoint = "/images";
+    constructor() {
+      super();
+    }
+    /**
+     * portage d'une fonction herité avec traitement
+     * suplementaire que celle herité et deja exposé par l'heritage du extends
+     * @param {Function} params  predicat du find
+     */
+    find(params) {
+      console.log(params);
+      super.find(params);
+    }
+    /**
+     * chargement de la liste d'images a partir du serveur REST
+     * @returns {Promise<Images>} promise<Images> (deja lu) du fetch
+     */
+    load() {
+      return fetch(`http://localhost:5679${this.#endpoint}`, {
+        headers: { Accept: "application/json" },
         method: "GET",
-    })
-    then((r) => r.json());
-    then((a) => {
-        Object.assign(this,a);
-    });
-    return promise;
+      })
+        .then((r) => {
+          console.log(r);
+          return r.json();
+        })
+        .then((a) => Object.assign(this, a));
+    }
   }
-}
-const images = new Images();
-const promiseImage = images.load();
+  const images = new Images();
+  const promiseImage=images.load();

eb8611e90e67533e996fa385bbad6a903d00ce95
Wargrimm <allan.lulin@cegedim-srh.com>
2024-12-05 14:30:05 +0100

    création métier

---
+++ js/metier/meme.js
@@ -0,0 +1,28 @@
+class Meme{
+    id = undefined;
+    text = "undefined"
+    x = 0;
+    y = 10;
+    fontSize = 10;
+    fontWeight = 500;
+    underline = false;
+    italic = false;
+    color = "#FFFFFF";
+    imageId= -1;
+    #endpoint='/memes';
+    constructor() {
+        console.log("constructeur de meme")
+    }
+    save() {
+        console.log("save at"+this.#endpoint, this);
+
+    }
+    publicSave() {
+        console.log('public save');
+    }
+    privateSave() {
+        console.log('private save');
+    }
+}
+let meme = new Meme();
+meme.save();

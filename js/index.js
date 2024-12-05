67ca89e6c4861f759bd5fcdffd341fbedea44121
Wargrimm <allan.lulin@cegedim-srh.com>
2024-12-05 10:01:47 +0100

    modif index

--- js/index.js
+++ js/index.js
@@ -2,28 +2,28 @@
  * chargement de base de mon app
  */
 function loadingDOM() {
-    document.querySelector("#js-notification").remove();
-    document.querySelector("#header button").remove();
-    loadNavbarEvents();
-    //loadEditorEvent();
-  }
-  /**
-   * Fonction de chargement des events de navbar
-   */
-  function loadNavbarEvents() {
-    var aList = document.querySelectorAll("a");
-    // for(var i=0;i<aList.length;i++){
-    //   aList[i].addEventListener('click',function(evt){
-    //     evt.preventDefault();
-    //     console.log('navigate to ',evt.target.href);
-    //   })
-    // }
-    aList.forEach(function (element) {
-      element.addEventListener("click", (evt) => {
-        evt.preventDefault();
-        history.pushState(null,null,evt.target.href);
-      });
+  document.querySelector("#js-notification").remove();
+  document.querySelector("#header button").remove();
+  loadNavbarEvents();
+  //loadEditorEvent();
+}
+/**
+ * Fonction de chargement des events de navbar
+ */
+function loadNavbarEvents() {
+  var aList = document.querySelectorAll("a");
+  // for(var i=0;i<aList.length;i++){
+  //   aList[i].addEventListener('click',function(evt){
+  //     evt.preventDefault();
+  //     console.log('navigate to ',evt.target.href);
+  //   })
+  // }
+  aList.forEach(function (element) {
+    element.addEventListener("click", (evt) => {
+      evt.preventDefault();
+      router.navigate(evt.target.href);
     });
-  }
-  document.addEventListener("DOMContentLoaded", loadingDOM);
-  var router=new Router(document.getElementById('wrapper'));
+  });
+}
+document.addEventListener("DOMContentLoaded", loadingDOM);
+var router=new Router(document.getElementById('wrapper'));

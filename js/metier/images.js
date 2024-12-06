97d1d49e14765913049cc0e4efa3ed87104e6d3d
Wargrimm <allan.lulin@cegedim-srh.com>
2024-12-06 12:41:14 +0100

    image + meme

--- js/metier/images.js
+++ js/metier/images.js
@@ -1,32 +1,41 @@
 class Images extends Array {
-    #endpoint = "/images";
-    constructor() {
-      super();
-    }
-    /**
-     * portage d'une fonction herité avec traitement
-     * suplementaire que celle herité et deja exposé par l'heritage du extends
-     * @param {Function} params  predicat du find
-     */
-    find(params) {
-      console.log(params);
-      return super.find(params);
-    }
-    /**
-     * chargement de la liste d'images a partir du serveur REST
-     * @returns {Promise<Images>} promise<Images> (deja lu) du fetch
-     */
-    load() {
-      return fetch(`http://localhost:5679${this.#endpoint}`, {
-        headers: { Accept: "application/json" },
-        method: "GET",
-      })
-        .then((r) => {
-          console.log(r);
-          return r.json();
-        })
-        .then((a) => Object.assign(this, a));
-    }
+  #endpoint = "/images";
+  constructor() {
+    super();
   }
-  const images = new Images();
-  const promiseImage=images.load();
+  /**
+   * portage d'une fonction herité avec traitement
+   * suplementaire que celle herité et deja exposé par l'heritage du extends
+   * @param {Function} params  predicat du find
+   */
+  find(params) {
+    console.trace(params);
+    return super.find(params);
+  }
+  /**
+   * chargement de la liste d'images a partir du serveur REST
+   * @returns {Promise<Images>} promise<Images> (deja lu) du fetch
+   */
+  load() {
+    console.time("load-images");
+    return fetch(`http://localhost:5679${this.#endpoint}`, {
+      headers: { Accept: "application/json" },
+      method: "GET",
+    })
+      .then((r) => {
+        console.log(r);
+        return r.json();
+      })
+      .then((a) =>{
+        console.groupCollapsed('find Images');
+        console.trace(a);
+        console.table(a);
+        Object.assign(this, a);
+        console.timeEnd("load-images");
+        console.groupEnd();
+        return this;
+      });
+  }
+}
+const images = new Images();
+const promiseImages=images.load();
--- js/metier/meme.js
+++ js/metier/meme.js
@@ -2,51 +2,51 @@
  * class de gestion d'un meme
  */
 class Meme {
-    id = undefined;
-    text = "";
-    x = 0;
-    y = 10;
-    fontSize = 10;
-    fontWeight = 500;
-    underline = false;
-    italic = false;
-    color = "#FFFFFF";
-    imageId = -1;
-    #endpoint = "/memes";
-    /**
-     * constructor de meme
-     */
-    constructor() {
-      console.log("constructor de meme");
-    }
-    /**
-     * enregistrement (POST ou PUT en fonction de l'id)
-     * @returns {Promise} promise de retour (stream lu) du fetch de sauvgarde
-     */
-    save() {
-      const promise = fetch(
-        `http://localhost:5679${this.#endpoint}${
-          undefined !== this.id ? "/" + this.id : ""
-        }`,
-        {
-          method: undefined !== this.id ? "PUT" : "POST",
-          headers: {
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify(this),
-        }
-      ).then((r) => r.json());
-      promise.then((o) => {
-        Object.assign(this, o);
-      });
-      return promise;
-    }
-    publicSave() {
-      console.log("public saving");
-    }
-    #privateSave() {
-      console.log("private saving");
-    }
+  id = undefined;
+  text = "";
+  x = 0;
+  y = 10;
+  fontSize = 10;
+  fontWeight = 500;
+  underline = false;
+  italic = false;
+  color = "#FFFFFF";
+  imageId = -1;
+  #endpoint = "/memes";
+  /**
+   * constructor de meme
+   */
+  constructor() {
+    console.log("constructor de meme");
   }
-  let meme = new Meme();
-  //meme.save();
+  /**
+   * enregistrement (POST ou PUT en fonction de l'id)
+   * @returns {Promise} promise de retour (stream lu) du fetch de sauvgarde
+   */
+  save() {
+    const promise = fetch(
+      `http://localhost:5679${this.#endpoint}${
+        undefined !== this.id ? "/" + this.id : ""
+      }`,
+      {
+        method: undefined !== this.id ? "PUT" : "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(this),
+      }
+    ).then((r) => r.json());
+    promise.then((o) => {
+      Object.assign(this, o);
+    });
+    return promise;
+  }
+  publicSave() {
+    console.log("public saving");
+  }
+  #privateSave() {
+    console.log("private saving");
+  }
+}
+let meme = new Meme();
+//meme.save();
